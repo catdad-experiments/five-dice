cf3e3a42f280c1755bc92b93d495a4ab1ebe5751
Kiril Vatev <vatev.1@gmail.com>
2021-01-03 00:15:06 -0500

    completing super basic implementation
    minus bonuses for extra five of a kind

--- src/loader.js
+++ src/loader.js
@@ -28,14 +28,12 @@ const Control = ({ title, value: _val, setValue, maxValue: _max, incrementValue:
     <div>
     <span>${title}: </span>
     <span> ${_val === null ? '_' : _val} </span>
-    <button onclick=${onLess}>&lt;</button>
-    <button onclick=${onMore}>&gt;</button>
+    <button onclick=${onLess}>${'<'}</button>
+    <button onclick=${onMore}>${'>'}</button>
     <button onclick=${onZero}>0</button>
     </div>
   `;
 };
-
-//const useState = () => [null, () => {}];
 
 const App = () => {
   const [ ones, setOnes ] = useState(null);
@@ -45,14 +43,38 @@ const App = () => {
   const [ fives, setFives ] = useState(null);
   const [ sixes, setSixes ] = useState(null);
 
+  const [ threeKind, setThreeKind ] = useState(null);
+  const [ fourKind, setFourKind ] = useState(null);
+  const [ fullHouse, setFullHouse ] = useState(null);
+  const [ small, setSmall ] = useState(null);
+  const [ large, setLarge ] = useState(null);
+  const [ fiveKind, setFiveKind ] = useState(null);
+  const [ chance, setChance ] = useState(null);
+
+  const upperTotal = [ones, twos, threes, fours, fives, sixes].reduce((a, b) => (a || 0) + (b || 0));
+  const upperBonus = upperTotal >= 63 ? 35 : 0;
+  const lowerTotal = [threeKind, fourKind, fullHouse, small, large, fiveKind, chance].reduce((a, b) => (a || 0) + (b || 0));
+  const grandTotal = upperTotal + upperBonus + lowerTotal;
+
   return html`
-    <div>the app</div>
+    <div>Upper Section</div>
     <${Control} title=Ones maxValue=${5 * 1} incrementValue=1 setValue=${setOnes} value=${ones} />
     <${Control} title=Twos maxValue=${5 * 2} incrementValue=2 setValue=${setTwos} value=${twos} />
     <${Control} title=Threes maxValue=${5 * 3} incrementValue=3 setValue=${setThrees} value=${threes} />
     <${Control} title=Fours maxValue=${5 * 4} incrementValue=4 setValue=${setFours} value=${fours} />
     <${Control} title=Fives maxValue=${5 * 5} incrementValue=5 setValue=${setFives} value=${fives} />
     <${Control} title=Sixes maxValue=${5 * 6} incrementValue=6 setValue=${setSixes} value=${sixes} />
+    <div>Upper section total: ${upperTotal}</div>
+    <div>Upper section bonus (for a score of 63 or more): ${upperBonus}</div>
+    <${Control} title="Three of a kind" maxValue=${5 * 6} incrementValue=1 setValue=${setThreeKind} value=${threeKind} />
+    <${Control} title="Four of a kind" maxValue=${5 * 6} incrementValue=1 setValue=${setFourKind} value=${fourKind} />
+    <${Control} title="Full house" maxValue=${25} incrementValue=25 setValue=${setFullHouse} value=${fullHouse} />
+    <${Control} title="Small straight" maxValue=${30} incrementValue=30 setValue=${setSmall} value=${small} />
+    <${Control} title="Large straight" maxValue=${40} incrementValue=40 setValue=${setLarge} value=${large} />
+    <${Control} title="Five of a kind" maxValue=${50} incrementValue=50 setValue=${setFiveKind} value=${fiveKind} />
+    <${Control} title="Chance" maxValue=${5 * 6} incrementValue=1 setValue=${setChance} value=${chance} />
+    <div>Lower section total: ${lowerTotal}</div>
+    <div>Grant total: ${grandTotal}</div>
   `;
 };
 
